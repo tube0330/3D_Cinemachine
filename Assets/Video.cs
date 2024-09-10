b833a54006da1f7602773950050fafbd4da2695a
tube0330 <zien0516@naver.com>
2024-09-10 15:19:04 +0900

    .

--- Assets/Video.cs
+++ Assets/Video.cs
@@ -6,7 +6,7 @@ using UnityEngine.Video;
 public class Video : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public bool isPlaying = false;
+    bool isPlaying = false;
 
     public void ClickVideo(bool isPlaying)
     {
